a56a86f8f969afb94c7ce5d4acd40f3765253821
e <898828969@qq.com>
2021-01-24 17:43:41 +0800

    改造完成（包括网关）

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfigUtil.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfigUtil.java
@@ -31,8 +31,8 @@ public final class NacosConfigUtil {
     public static final String SYSTEM_DATA_ID_POSTFIX = "-system-rules";
     public static final String AUTHORITY_DATA_ID_POSTFIX = "-authority-rules";
     /* 网关限流规则 */
-    public static final String GATEWAY_API_DATA_ID_POSTFIX = "-gateway-api-rules";
-    public static final String GATEWAY_FLOW_DATA_ID_POSTFIX = "-gateway-flow-rules";
+    public static final String GATEWAY_API_DATA_ID_POSTFIX = "-gw-flow";
+    public static final String GATEWAY_FLOW_DATA_ID_POSTFIX = "-gw-api-group";
 
     /**
      * cc for `cluster-client`
